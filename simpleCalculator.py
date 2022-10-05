886453392560177013c41efe5b115a41b4fca599
MD ASHIQUR RAHMAN <77934150+MdAshiqurRahman98@users.noreply.github.com>
2022-10-06 00:07:16 +0600

    Committed

---
+++ simpleCalculator.py
@@ -0,0 +1,20 @@
+# Simple Calculator
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("     ")
+add = num1 + num2
+print("The sum is: ", add)
+
+sub = num1 - num2
+print("The difference is: ", sub)
+
+mul = num1 * num2
+print("The product is: ", mul)
+
+div = num1 / num2
+print("The quotient is: ", div)
+
+mod = num1 % num2
+print("The remainder is: ", mod)
